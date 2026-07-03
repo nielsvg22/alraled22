37bcc6516e07b1588357c1db2c499e921e8fe1ce
Niels <nielsvangortel@autohopper.nl>
2026-07-03 09:30:02 +0200

    fix: remove unused PAYMENT_METHODS constant

--- aldraled/src/pages/ProductDetail.js
+++ aldraled/src/pages/ProductDetail.js
@@ -36,8 +36,6 @@ const TRUST_BADGES = [
   { icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z', label: 'Gratis verzending', sub: 'Boven €250,-' },
 ];
 
-const PAYMENT_METHODS = ['ideal', 'visa', 'mastercard', 'bancontact', 'paypal'];
-
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct]           = useState(null);
